8c3de816a999294bab5c339c64b01b1b7596802b
Jonathan Samuel <jonathansamuel2k@gmail.com>
2025-04-09 18:36:09 +0530

    api: Add player apis

--- osu/api.py
+++ osu/api.py
@@ -1,9 +1,11 @@
 from ninja import NinjaAPI
 from ninja.security import django_auth
 
+from osu.player.api import router as player_router
 from osu.user.api import router as user_router
 
 api = NinjaAPI(auth=django_auth, csrf=True)
 
 # Routers
 api.add_router("/user", user_router)
+api.add_router("/players", player_router)
