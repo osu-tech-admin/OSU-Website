7cb6c975889aa3116f0b3a6b798df5cbed03bc0b
Jonathan Samuel <jonathansamuel2k@gmail.com>
2025-04-09 10:43:45 +0530

    deploy: Setup

---
+++ backend/production.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+
+import dj_database_url
+
+from backend.settings import *  # noqa: F403
+
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = bool(int(os.environ.get("SECURE_SSL_REDIRECT", "1")))
+SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)  # noqa: F405
+EMAIL_INVITATION_BASE_URL = "https://osu-web.fly.dev"
+FRONTEND_URL = "https://osu-web.fly.dev"
+
+DATA_DIR = Path("/data")
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config()  # type: ignore[assignment]  # noqa: F405
+else:
+    DATABASES["default"]["NAME"] = DATA_DIR / "production.db.sqlite"  # noqa: F405
+MEDIA_ROOT = DATA_DIR / "media"
+MEDIA_URL = "/media/"
+
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
+if SENTRY_DSN:
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        enable_tracing=True,
+    )
---
+++ deploy/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Start services
+sudo nginx
+# sudo cron
+
+# Setup env vars for cron jobs
+# HERE=$(dirname "$0")
+# "$HERE/make_cron_env.py"
+
+# Migrate DB
+python manage.py migrate
+
+# Ensure no security check errors
+python manage.py check --deploy
+
+# Start the server using gunicorn
+export PATH="$HOME/.local/bin:$PATH"
+gunicorn -w 4 backend.wsgi
