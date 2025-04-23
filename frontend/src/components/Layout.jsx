f4556d22cfd3f0eba58f916cf65cfaa73d397b73
SibiAkkash <sibi.akkash@gmail.com>
2025-04-23 23:36:43 +0530

    layout: Add padding to children only

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout(props) {
   return (
     <div class="flex min-h-screen flex-col">
       <Header />
-      <main class="flex-1">{props.children}</main>
+      <main class="flex-1 mx-2">{props.children}</main>
       <footer class="bg-background py-6 text-center text-sm text-muted-foreground">
         <div class="container">
           <p>
